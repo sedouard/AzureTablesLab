6559ff36d32cb425712e04454a7d3ba937155228
sedouard <steven.edouard1@gmail.com>
2014-03-20 14:26:50 -0700

    Completed demo code

--- AzureTablesLab/Program.cs
+++ AzureTablesLab/Program.cs
@@ -12,6 +12,9 @@ namespace AzureTablesLab
             DoSingleInsert();
             DoBatchInsert();
             GetAllInParition();
+            ReplaceEntity();
+            InsertOrReplaceEntity();
+            GetRangeOfEntitiesInParition();
         }
 
         //Does single insert of entity
@@ -90,6 +93,136 @@ namespace AzureTablesLab
                 Console.WriteLine("{0}, {1}\t{2}\t{3}", entity.PartitionKey, entity.RowKey, entity.Email, entity.PhoneNumber);
             }
 
+        }
+
+        public static void ReplaceEntity()
+        {
+            // Retrieve the storage account from the connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the table client
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Create the CloudTable object that represents the "people" table.
+            CloudTable table = tableClient.GetTableReference("people");
+
+            // Create a retrieve operation that takes a customer entity.
+            TableOperation retrieveOperation = TableOperation.Retrieve<CustomerEntity>("Smith", "Ben");
+
+            // Execute the operation.
+            TableResult retrievedResult = table.Execute(retrieveOperation);
+
+            // Assign the result to a CustomerEntity object.
+            CustomerEntity updateEntity = (CustomerEntity)retrievedResult.Result;
+
+            if (updateEntity != null)
+            {
+                // Change the phone number.
+                updateEntity.PhoneNumber = "425-555-0105";
+
+                // Create the Replace TableOperation
+                TableOperation updateOperation = TableOperation.Replace(updateEntity);
+
+                // Execute the operation.
+                table.Execute(updateOperation);
+
+                Console.WriteLine("Entity updated.");
+            }
+
+            else
+                Console.WriteLine("Entity could not be retrieved.");
+        }
+
+        public static void InsertOrReplaceEntity()
+        {
+            // Retrieve the storage account from the connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Create the CloudTable object that represents the "people" table.
+            CloudTable table = tableClient.GetTableReference("people");
+
+            // Create a retrieve operation that takes a customer entity.
+            TableOperation retrieveOperation = TableOperation.Retrieve<CustomerEntity>("Smith", "Ben");
+
+            // Execute the operation.
+            TableResult retrievedResult = table.Execute(retrieveOperation);
+
+            // Assign the result to a CustomerEntity object.
+            CustomerEntity updateEntity = (CustomerEntity)retrievedResult.Result;
+
+            if (updateEntity != null)
+            {
+                // Change the phone number.
+                updateEntity.PhoneNumber = "425-555-1234";
+
+                // Create the InsertOrReplace TableOperation
+                TableOperation insertOrReplaceOperation = TableOperation.InsertOrReplace(updateEntity);
+
+                // Execute the operation.
+                table.Execute(insertOrReplaceOperation);
+
+                Console.WriteLine("Entity was updated.");
+            }
+
+            else
+                Console.WriteLine("Entity could not be retrieved.");
+        }
+
+        public static void GetSingleEntity()
+        {
+            // Retrieve the storage account from the connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Create the CloudTable object that represents the "people" table.
+            CloudTable table = tableClient.GetTableReference("people");
+
+            // Create a retrieve operation that takes a customer entity.
+            TableOperation retrieveOperation = TableOperation.Retrieve<CustomerEntity>("Smith", "Ben");
+
+            // Execute the retrieve operation.
+            TableResult retrievedResult = table.Execute(retrieveOperation);
+
+            // Print the phone number of the result.
+            if (retrievedResult.Result != null)
+                Console.WriteLine(((CustomerEntity)retrievedResult.Result).PhoneNumber);
+            else
+                Console.WriteLine("The phone number could not be retrieved.");
+        }
+
+        public static void GetRangeOfEntitiesInParition()
+        {
+            // Retrieve the storage account from the connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            //Create the CloudTable object that represents the "people" table.
+            CloudTable table = tableClient.GetTableReference("people");
+
+            // Create the table query.
+            TableQuery<CustomerEntity> rangeQuery = new TableQuery<CustomerEntity>().Where(
+                TableQuery.CombineFilters(
+                    TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "Smith"),
+                    TableOperators.And,
+                    TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.LessThan, "E")));
+
+            // Loop through the results, displaying information about the entity.
+            foreach (CustomerEntity entity in table.ExecuteQuery(rangeQuery))
+            {
+                Console.WriteLine("{0}, {1}\t{2}\t{3}", entity.PartitionKey, entity.RowKey,
+                    entity.Email, entity.PhoneNumber);
+            }
         }
     }
 
